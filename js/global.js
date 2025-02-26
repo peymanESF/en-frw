5b7165f4e042c0491bdcf70d5fe051d659a7c43c
p.esfandmazd <p.esfandmazd@douran.ir>
2025-02-26 12:34:08 +0330

    add topBtn scroll

--- js/global.js
+++ js/global.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
     if (tabName.toLowerCase() == $(this).text().toLowerCase() && tabName.toLowerCase() != "home")
       $(this).addClass("currentPage");
   });
+  $(window).scroll(function () {
+    $(window).scrollTop() > 777 ? $("#totop-btn").addClass("active") : $("#totop-btn").removeClass("active")
+  })
+  $("#totop-btn").click(scrollTop);
+
+  function scrollTop() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+  }
 });
