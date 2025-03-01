cb81fbb9c8580750e0bb55682436770bc7859b4e
Peyman esfandmazd <p.esfandmazd313@gmail.com>
2025-03-02 00:12:39 +0330

    start implement home page gallery section.edit mobile menu drop-down hover.

--- js/theme.js
+++ js/theme.js
@@ -4,6 +4,39 @@ $(document).ready(function () {
     window.location.href = newsLink;
   });
 
+  
+
+  $(".video-card").click(function () {
+    const videoUrl = $(this).data("video");
+    $(".popup-overlay video").attr("src", videoUrl);
+    $(".popup-overlay").fadeIn();
+  });
+
+  $(".close-btn").click(function () {
+    $(".popup-overlay").fadeOut();
+    $(".popup-overlay video").trigger("pause");
+  });
+  $(".play-video").on("click", function () {
+    const videoSrc = $(this).data("video");
+    $("#popupVideo").attr("src", videoSrc); // تنظیم URL ویدئو
+    $("#videoPopup").addClass("active"); // نمایش پاپ‌آپ
+    $("#popupVideo")[0].play(); // شروع پخش ویدئو
+});
+
+$(".video-popup-close").on("click", function () {
+    $("#videoPopup").removeClass("active"); // بستن پاپ‌آپ
+    $("#popupVideo")[0].pause(); // توقف پخش ویدئو
+    $("#popupVideo").attr("src", ""); // پاک کردن URL برای ریست
+});
+
+// بستن با کلیک روی بک‌گراند
+$("#videoPopup").on("click", function (e) {
+    if (e.target === this) {
+        $(this).removeClass("active");
+        $("#popupVideo")[0].pause();
+        $("#popupVideo").attr("src", "");
+    }
+});
   /*
   $(window).on("scroll", function () {
     let scrollPos = $(document).scrollTop() + 130; // کمی آفست برای بهتر کار کردن
@@ -22,7 +55,6 @@ $(document).ready(function () {
     });
   });
   */
-
 
   /*
     // Smooth Scroll برای کلیک روی آیتم‌های منو
@@ -99,4 +131,13 @@ $(document).ready(function () {
     $(".custom-dots button").removeClass("active");
     $(".custom-dots button").eq(currentIndex).addClass("active");
   }
+});
+$(window).on("load", function () {
+  $(".gallery-owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+  });
 });
